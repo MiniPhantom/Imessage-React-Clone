f3b778d9fe76bfdda4b4f2d40f14498d9571ce34
MiniPhantom <84849044+MiniPhantom@users.noreply.github.com>
2022-09-16 13:18:17 +0800

    Update firebase.js

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDVT0vSY_hulb6Fg7oRwIjGLiPjgl_HB2M",
-  authDomain: "imessage-react-clone.firebaseapp.com",
-  databaseURL: "https://imessage-react-clone.firebaseio.com",
-  projectId: "imessage-react-clone",
-  storageBucket: "imessage-react-clone.appspot.com",
-  messagingSenderId: "316303069720",
-  appId: "1:316303069720:web:a427dec328821e53e2296d"
+ const firebaseConfig = {
+  apiKey: "AIzaSyCuVqKpS1piBst2KR_jT-j5R5vjHJc2Lds",
+  authDomain: "chatroom-api-key-test-1.firebaseapp.com",
+  projectId: "chatroom-api-key-test-1",
+  storageBucket: "chatroom-api-key-test-1.appspot.com",
+  messagingSenderId: "1076786971919",
+  appId: "1:1076786971919:web:4da1be4a056a9d9a366a7a"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
